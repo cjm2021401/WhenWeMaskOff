212c53f2af2c757e732950760ca20f4cbf40df79
mayf <cjm2021401@khu.ac.kr>
2021-08-15 15:36:25 +0900

    FEAT : Build SpringBoot
    -

---
+++ src/main/java/COVID19/WhenWeMaskOff/WhenWeMaskOffApplication.java
@@ -0,0 +1,13 @@
+package COVID19.WhenWeMaskOff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhenWeMaskOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhenWeMaskOffApplication.class, args);
+	}
+
+}
